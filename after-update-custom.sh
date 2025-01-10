dca91c0a2df6ed708e77df9e79ddda0186095dc8
tlez1212 <tlez1212@gmail.com>
2025-01-10 09:29:30 +0800

    Update after-update-custom.sh
    modify default ip

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -2,8 +2,8 @@
 # Description: (After Update feeds)
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # Set DHCP IP Address start and end
-sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
-sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
+#sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
+#sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
