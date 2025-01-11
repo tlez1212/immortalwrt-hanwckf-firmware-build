819551b5aa0195d7d7890040fdd4217023fb2845
tlez1212 <tlez1212@gmail.com>
2025-01-11 22:56:35 +0800

    Update before-update-custom.sh

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -1,12 +1,10 @@
 # Description: (Before Update feeds)
-
-
-# Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
-# echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" > "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+
+echo "src-git-full packages https://github.com/immortalwrt/packages.git;openwrt-21.02" >> "feeds.conf.default"
+echo "src-git-full luci https://github.com/immortalwrt/luci.git;openwrt-21.02" >> "feeds.conf.default"
+echo "src-git-full routing https://github.com/openwrt/routing.git;openwrt-21.02" >> "feeds.conf.default"
+echo "src-git-full telephony https://github.com/openwrt/telephony.git;openwrt-21.02" >> "feeds.conf.default"
