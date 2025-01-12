9db9b2a0e26fdf831051bd12572b90498b5eb818
tlez1212 <tlez1212@gmail.com>
2025-01-12 13:54:26 +0800

    Update before-update-custom.sh

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -1,10 +1,5 @@
 # Description: (Before Update feeds)
 # Add a feed source
 
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" > "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
-
-echo "src-git-full packages https://github.com/immortalwrt/packages.git;openwrt-21.02" >> "feeds.conf.default"
-echo "src-git-full luci https://github.com/immortalwrt/luci.git;openwrt-21.02" >> "feeds.conf.default"
-echo "src-git-full routing https://github.com/openwrt/routing.git;openwrt-21.02" >> "feeds.conf.default"
-echo "src-git-full telephony https://github.com/openwrt/telephony.git;openwrt-21.02" >> "feeds.conf.default"
